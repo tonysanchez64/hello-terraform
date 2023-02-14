1e8677d7310dcc4c4d7cd554fd3db511ff43f3f1
antonio <tony64kx.me@gmail.com>
2023-02-14 16:34:37 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-terraform{
-     required_providers{
-        aws = {
-	  source = "hashicorp/aws"
-	  version = "~> 4.16"
-	}
-     }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-     required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 
 }
 
@@ -18,7 +18,7 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
 
-  ami = "ami-0b752bf1df193a6c4"
+  ami           = "ami-0b752bf1df193a6c4"
   instance_type = "t2.micro"
 
   tags = {
