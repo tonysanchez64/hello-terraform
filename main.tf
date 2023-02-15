9460d60f9c39b84fdebed5422ac827c9c68f5096
antonio <tony64kx.me@gmail.com>
2023-02-15 13:37:47 +0100

    merge ansible and terraformi

--- main.tf
+++ main.tf
@@ -28,29 +28,12 @@ resource "aws_instance" "app_server" {
 
   tags = {
     Name = var.instance_name
-  }
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
-    private_key = file("/home/sinensia/.ssh/clave-lucatic.pem")
+    APP  = "vue2048"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install -y httpd",
-      "sudo systemctl start httpd",
-      "sudo systemctl enable httpd",
-      "sudo chown -R ec2-user /var/www/html",
-    ]
-  }
-  provisioner "file" {
-    source      = "../hello-2048/public_html/"
-    destination = "/var/www/html"
+  provisioner "local-exec" {
+    command = "ansible-playbook -i aws_ec2.yml ec2.yml"
   }
 
 }
-
-
-
 
