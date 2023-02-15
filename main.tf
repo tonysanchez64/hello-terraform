d45b8534d76c505f3a6313047d21243707b0e643
antonio <tony64kx.me@gmail.com>
2023-02-15 08:19:02 +0100

    added modifications to main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,11 @@ resource "aws_instance" "app_server" {
 
   ami           = "ami-0b752bf1df193a6c4"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    "sg-0f3c5d809a2d46c67",
+  ]
+  subnet_id = "subnet-06425a67e3f5d820a"
+  key_name  = "clave-lucatic"
 
   tags = {
     Name = "Hello_terraform"
